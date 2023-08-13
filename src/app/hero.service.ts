56a8bceb6d264ddb85b91954e68cb585a3988fb3
andreaDiClaudio <andreadicla@gmail.com>
2023-08-13 15:38:18 +0200

    error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,10 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
 //This marks the class as one that participates in the dependency injection system.
 //Removing data access from components means you can change your mind about the implementation anytime, without touching any components. They don't know how the service works.
 @Injectable({
@@ -13,23 +17,51 @@ import { MessageService } from './message.service';
 
 export class HeroService {
 
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
+  /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => console.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-
-    //of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
-    const hero = HEROES.find(h => h.id === id)!;
-
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      //The RxJS tap() operator enables this ability by looking at the observable values, doing something with those values, and passing them along. The tap() callback doesn't access the values themselves.
+      tap(_ => console.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   //This is an example of a typical service-in-service scenario in which you inject the MessageService into the HeroService which is injected into the HeroesComponent.
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
 
 }
